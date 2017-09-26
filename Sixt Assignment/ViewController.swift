bce38c1540fc9b298d77cddffb391d511ef71d16
Arup Saha <arup.saha@timesinternet.in>
2017-09-26 20:32:39 +0530

    Initial Commit

---
+++ Sixt Assignment/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Sixt Assignment
+//
+//  Created by Arup Saha on 9/26/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
